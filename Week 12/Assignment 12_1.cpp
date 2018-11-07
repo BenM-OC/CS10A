be976ff6d4a192abd630c2372df24207d7e1506f
Benjamin O'Connor <benoc.formal@gmail.com>
2018-11-07 15:24:12 -0800

    Added Week 12 Assignment 1, which builds on Week 11's knowledge to add user defined aspects to the random equation generator

---
+++ Week 12/Assignment 12_1.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <iomanip> 
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	int numProbsPerSet;
+
+	srand(static_cast<unsigned>(time(0))); 
+
+	getProbsPerSet(numProbsPerSet);  
+	doOneSet('x', numProbsPerSet); // no more than three args
+	doOneSet('-', numProbsPerSet); // no more than three args
+	doOneSet('*', numProbsPerSet); // no more than three args
+	printReport();
+}
+
+
+
+
+
+
+void getProbsPerSet(int &numProbsPerSet) 
+{
+	cout << "Enter the number of problems you would like in each set: ";
+	cin >> numProbsPerSet;
+}
+
+
+
+
+
+void doOneSet(char operationSymbol, int numProbsPerSet)
+{
+
+	int firstNum, secondNum, correctAnswer, maxOperandNum;
+
+	for(int setIterationCounter = 0; setIterationCounter < numProbsPerSet; setIterationCounter++){
+		printHeader(setIterationCounter);
+		getMaxNum(maxOperandNum);
+		doOneProblem(operationSymbol, maxOperandNum);
+	}
+}
+
+
+
+
+
+void printHeader(int setIterationCounter);
+{
+	for (setIterationCounter = 1; setIterationCounter < 4; setIterationCounter++){
+		cout << "Set #" << setIterationCounter << endl;
+		cout << "----------" << endl;
+	}
+}
+
+
+
+
+
+void getMaxNum(int &maxOperandNum)
+{
+
+	cout << "What is the maximum operand number for this set?: " ;
+	cin >> maxOperandNum;
+}
+
+
+
+
+
+void doOneProblem(char operationSymbol, int maxOperandNum)
+{
+	int firstNum, secondNum, correctAnswer;
+
+	generateOperands(firstNum, secondNum, maxOperandNum);
+	calculateCorrectAnswer(firstNum, secondNum, correctAnswer, operationSymbol);
+	checkAnswer(correctAnswer);
+}
+
+
+
+
+
+void generateOperands(int &firstNum, int &secondNum, int maxOperandNum)
+{
+	int randNum;
+	
+	randNum = rand() % maxOperandNum;
+    firstNum = randNum;
+    randNum = rand() % maxOperandNum;
+    secondNum = randNum;
+}
+
+
+
+
+
+
+void calculateCorrectAnswer(int firstNum, int secondNum, int &correctAnswer, char operationSymbol)
+{
+	switch (operationSymbol){
+		case '+' : 
+			correctAnswer = firstNum + secondNum;
+			cout << firstNum << " + " << secondNum << " = ";
+			break;
+
+		case '-' : 
+			correctAnswer = firstNum - secondNum;
+			cout << firstNum << " - " << secondNum << " = ";
+			break;
+
+		case '*' : 
+			correctAnswer = firstNum * secondNum;
+			cout << firstNum << " * " << secondNum << " = ";
+			break;
+
+		case '/' : 
+			correctAnswer = firstNum - secondNum;
+			cout << firstNum << " / " << secondNum << " = ";
+			break;
+	}
+}
+
+
+
+
+
+
+void checkAnswer(int correctAnswer)
+{
+	int usersAnswer;
+	cin >> usersAnswer;
+
+    if(usersAnswer == correctAnswer){
+    	cout << "Correct";
+    } else {
+    	cout << "Incorrect";
+    }
+
+    cout << endl;
+}
+
+
+
+
+
+
+void printReport()
+{
+	
+}
